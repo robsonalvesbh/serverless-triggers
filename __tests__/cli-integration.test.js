9c4ef92a5c8f247ed924f616a433e9af57856578
Robson alves <robson.alves@olxbr.com>
2020-03-27 00:35:43 -0300

    fix initial tests

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -1,6 +1,7 @@
 const { system, filesystem } = require('gluegun')
 
 const src = filesystem.path(__dirname, '..')
+const { version: packageVersion } = require('./../package.json')
 
 const cli = async cmd =>
   system.run(
@@ -9,23 +10,10 @@ const cli = async cmd =>
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(packageVersion)
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
-
-test('generates file', async () => {
-  const output = await cli('generate foo')
-
-  expect(output).toContain('Generated file at models/foo-model.js')
-  const foomodel = filesystem.read('models/foo-model.js')
-
-  expect(foomodel).toContain(`module.exports = {`)
-  expect(foomodel).toContain(`name: 'foo'`)
-
-  // cleanup artifact
-  filesystem.remove('models')
+  expect(output).toContain(packageVersion)
 })
