53ea04d4d78070c65c766947aee99f9141617141
Robson alves <robson.alves@olxbr.com>
2020-03-31 00:36:07 -0300

    add init command

---
+++ src/commands/init.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: 'init',
+  description: 'initialize payload folders',
+  alias: ['i'],
+  run: async toolbox => {
+    const {
+      template: { generate },
+      filesystem,
+      print: { info, success, error },
+      getPayloadsFolder
+    } = toolbox
+
+    info('Initializing...')
+
+    const { separator } = filesystem
+
+    try {
+      const payloadsFolder = getPayloadsFolder()
+
+      await generate({
+        template: `payload.js.ejs`,
+        target: `${payloadsFolder}${separator}example.json`
+      })
+
+      success(`Generated tests/payloads folder`)
+      success(`Generated example file in tests/payloads`)
+    } catch (e) {
+      error(`Unexpected error occurred on initializing files`)
+    }
+  }
+}
