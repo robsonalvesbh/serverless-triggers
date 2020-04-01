9ec6a4c54503e2c07ca0d76bf5bf3b4d3f1a7860
Robson alves <robson.alves@olxbr.com>
2020-04-01 00:42:54 -0300

    create template helper extensions

---
+++ src/extensions/templates.js
@@ -0,0 +1,15 @@
+module.exports = toolbox => {
+  
+  const templatesAvailable = [
+    'sqs',
+    'sns',
+    'sns-to-sqs'
+  ]
+
+  toolbox.isTemplateValid = (template) => {
+    if (template === undefined) return false
+
+    return templatesAvailable.some(templateName => templateName === template.toLowerCase())
+  }
+    
+}
